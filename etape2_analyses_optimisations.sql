9db64a3ee35c55ea5a5def0c601490038ba09ea3
Talida Font <talida.font@esme.fr>
2025-12-16 18:54:17 +0100

    Ajout partie 2

---
+++ etape2_analyses_optimisations.sql
@@ -0,0 +1,12 @@
+-- =====================================================
+-- ETAPE 2 : ANALYSES ET OPTIMISATIONS SUPPLÉMENTAIRES
+-- =====================================================
+
+USE ROBOTS3;
+
+-- =====================================================
+-- 1. MÉTRIQUES AVANCÉES
+-- =====================================================
+
+-- 1.1 Utilise directement vue_indicateurs_performance pour les durées
+-- SELECT * FROM vue_indicateurs_performance;
